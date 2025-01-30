105ff117035b71b4b190558535b9080cad9e5abe
Jolene <joleneodia@hotmail.com>
2025-01-30 14:25:39 -0500

    updated js file

--- In-Class Tasks/Week 3 - Events/js/3_events.js
+++ In-Class Tasks/Week 3 - Events/js/3_events.js
@@ -1,4 +1,15 @@
 window.onload = setup;
-function setup(){
+function setup() {
     console.log("events!")
+
+    let introSection = document.querySelector("#intro");
+  
+    introSection.addEventListener("click", function (e) {
+        console.log(this);
+        console.log(e)
+
+        //a:
+        this.style.background = `rgba(214, 110, 239, 0.5)`
+    });
 }
+
